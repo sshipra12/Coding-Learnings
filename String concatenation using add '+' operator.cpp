5edf4e76cf7dbdbc38a0b4dcf8fd1b3e82b3e5d7
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-05 11:47:26 +0530

    Create String concatenation using add '+' operator.cpp

---
+++ String concatenation using add '+' operator.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str,s;
+    cin>>str;
+
+    //Case 1:
+    s=str;  //this works
+
+    //Case2:
+    str=s[0]+s[1];     //this won't work since '+' means concatenate and it requires 
+    str=""+s[0]+s[1];  //base string in which we want to add/concatenate
+                       //in 2nd case, base string("") in which we are concatenating is not present/placed
+
+    //Case3:
+    str=str+s[0]+s[1];  //this works because we provide the base string (here 'str') which is present
+                        // in which we want to add/concatenate
+
+    cout<<str;
+}
