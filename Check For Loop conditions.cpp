293b06880825101af068495d32c9bb466146bc23
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-08-25 22:35:10 +0530

    Create Check For Loop conditions.cpp

---
+++ Check For Loop conditions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int i,j,smaller;
+        vector<int> ans;
+        for(i=0;i<nums.size();i++){
+            smaller=0;
+            for(j=0;j<nums.size();j++){
+                if(j!=i&&nums[j]<nums[i])
+                    smaller++;
+            }
+            ans.push_back(smaller);
+        }
+        return ans;
+
+        /*for(i=0;i<nums.size()&&j!=i;i++){    // Here condition (j!=i) is in loop conditions so the FIRST time condition fails WHOLE loop will exit
+            smaller=0;
+            for(j=0;j<nums.size();j++){
+                if(nums[j]<nums[i])
+                    smaller++;
+            }
+            ans.push_back(smaller);
+        }*/
+    }
+};
