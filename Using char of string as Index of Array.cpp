6cd7d4eccaabbd84f266c208e435a02fa0df578d
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2023-10-06 22:37:40 +0530

    Update Using char of string as Index of Array.cpp

--- Using char of string as Index of Array.cpp
+++ Using char of string as Index of Array.cpp
@@ -4,7 +4,7 @@ public:
         int i;
         // LEETCODE Problem : Check if the Sentence Is Pangram (i.e check if string contains all 26 alphabets)
       
-        vector<bool> alphabets(26,false);   //array of size 26 to store presence of alphabets(0 to 25 denoting a to z)
+        vector<bool> alphabets(26,false);   // array of size 26 to store presence of alphabets(0 to 25 denoting a to z)
       
         //we create 26 size array to check which alphabets are present in string
         //we iterate over the given string and use each char in string 
