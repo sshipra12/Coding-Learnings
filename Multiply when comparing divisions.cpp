2c77403c205e71e05f462204fa13f435510fcd54
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-12-12 21:32:23 +0530

    Update Multiply when comparing divisions.cpp

--- Multiply when comparing divisions.cpp
+++ Multiply when comparing divisions.cpp
@@ -11,12 +11,12 @@ int main()
     denominator = 100;
 
     //Case1:
-    if(num>=(numerator/denominator))  // is 1 >= 1.25 , but due to int type conversion 1.25 will convert to 1
-                                      // and this condition becomes true , which is not correct
+    if(num>=(numerator/denominator))   // is 1 >= 1.25 , but due to int type conversion 1.25 will convert to 1
+                                       // and this condition becomes true , which is not correct
         cout<<"Yes";
 
     //Case2:
-    if(num*denominator>=numerator)   // here comparison is like this 100 >= 125 and this condition becomes false, which is accurate
-                                     // instead of comparing by division, we cross multiply the denominators and then compare
+    if(num*denominator>=numerator)    // here comparison is like this 100 >= 125 and this condition becomes false, which is accurate
+                                      // instead of comparing by division, we cross multiply the denominators and then compare
         cout<<"Yes";
 }
