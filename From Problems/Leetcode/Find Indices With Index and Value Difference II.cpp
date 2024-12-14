bb2302e9d3682653d818eb2ba9a89d50725fb227
Shipra Sinha <141357738+sshipra12@users.noreply.github.com>
2024-12-14 19:46:59 +0530

    Update Find Indices With Index and Value Difference II.cpp

--- From Problems/Leetcode/Find Indices With Index and Value Difference II.cpp
+++ From Problems/Leetcode/Find Indices With Index and Value Difference II.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int i,j,n=nums.size(),segMax,segMin;
+        vector<int> ans(2,-1);
+        map<int,vector<int>> segPositionsMap;
+
+        // USING Power of hash maps, as we are storing an array as value in key-value pair in map
+        // fill map first from indexDifference to n-1 segment storing each element's positions
+        for(i=n-1;i>=indexDifference;i--)
+            segPositionsMap[nums[i]].push_back(i);
+
+        
+        for(i=0;i<=n-1&&i+indexDifference<=n-1;i++){
+            // adjust the valid segment stored in map accroding to ith element
+            if(i!=0){
+                segPositionsMap[nums[i+indexDifference-1]].pop_back();
+                if(segPositionsMap[nums[i+indexDifference-1]].empty())
+                    segPositionsMap.erase(nums[i+indexDifference-1]);
+            }
+            
+            // valid segment for comparision is from i+indexDifference to n-1
+            segMax=segPositionsMap.rbegin()->first;
+            segMin=segPositionsMap.begin()->first;
+
+            if(abs(nums[i]-segMax)>=valueDifference){
+                ans[0]=i;
+                ans[1]=segPositionsMap[segMax][0];
+                break;
+            }
+            else if(abs(nums[i]-segMin)>=valueDifference){
+                ans[0]=i;
+                ans[1]=segPositionsMap[segMin][0];
+                break;
+            }
+
+            if(ans[0]!=-1)
+                break;
+        }
+        return ans;
+    }
+};
+//OLD Solution
+/*
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
         // TODO: This solution gives TLE, resolve this
         int i,j,n=nums.size(),lMax=0,lMin=INT_MAX; // lMax and lMin to store the left min and max while iterating with index 'i'
         vector<int> ans;
@@ -26,3 +70,4 @@ public:
         return ans;
     }
 };
+*/
